730417aa22606bf2cddfacaf0cb28c8679c616d7
Nicole Torres <nicole.torres@mscoding.org>
2024-08-07 15:57:43 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+//VARIABLES
+let coin_arr = ["pennies", "nickles", "dimes", "quarters"];
+let coinVal_arr = [0.01, 0.05, 0.1, 0.25];
+let coinAmount_arr = [100, 75, 78, 45];
+let wrapper_arr = [50, 40, 50, 40];
+let totalVal_arr = [0, 0, 0, 0];
+
+//Calculate each type of coin in dollars
+
+//Calculate total value combined of coins
+
+//SortDisplay # amount of wrappers needed
+
+//Display total amount of coins in dollars
